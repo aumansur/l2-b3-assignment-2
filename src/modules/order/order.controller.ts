e7cae2e8aac77ed76fd6b34d6c53692739e9db31
Md. Mansur <ahammadullah2001@gmail.com>
2024-05-22 11:39:06 +0600

    product model and interface created successfully

--- src/modules/order/order.controller.ts
+++ src/modules/order/order.controller.ts
@@ -1,12 +1,35 @@
 import { Request, Response } from "express";
 import { OrderService } from "./order.service";
 import OrderValidationSchema from "./order.validation.zod";
+import { Product } from "../products/product.model";
 // order create
 const orderCreate = async (req: Request, res: Response) => {
   try {
     const orderData = req.body;
-    // zod validation
     const OrderValidation = OrderValidationSchema.parse(orderData);
+    const { error } = req.body;
+    if (error)
+      return res
+        .status(400)
+        .json({ success: false, message: error.details[0].message });
+
+    const product = await Product.findById(req.body.productId);
+    if (!product)
+      return res
+        .status(404)
+        .json({ success: false, message: "Product not found" });
+
+    if (product.inventory.quantity < req.body.quantity) {
+      return res.status(400).json({
+        success: false,
+        message: "Insufficient quantity available in inventory",
+      });
+    }
+
+    //=====================
+
+    // zod validation
+
     const result = await OrderService.orderCreate(OrderValidation);
     res.status(200).json({
       success: true,
